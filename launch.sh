42dd10467bec8867a579572e06dc86bb1b177516
yu-it <yousuke0303@gmail.com>
2017-12-29 22:23:24 +0900

    commit

--- launch.sh
+++ launch.sh
@@ -1,12 +1,17 @@
 current=$(pwd)
 
 cd /home/pi/bin/programs/camera_inst/mjpg-streamer/mjpg-streamer-experimental
-nohup ./mjpg_streamer -i "./input_raspicam.so -fps 10 -x 320 -y 240" -o "./output_http.so -w ./www -p 8080" &
+nohup ./mjpg_streamer -i "./input_raspicam.so -fps 10 -x 320 -y 240" -o "./output_http.so -w ./www -p 8080" > raspin_cam.log 2> raspin_cam_error.log &
 cd $current
 cd ../raspin
-nohup npm start &
+nohup npm start > npm_raspin.log 2> npm_raspin_error.log &
 sleep 10
 cd ../physical_computing.raspberry_zero.raspin_client
-nohup sudo python sensor.py &
-nohup sudo python computing_resource_monitor.py &
-nohup sudo python physical_controller.py &
+if [ -e ./raspin ]; then
+ ln -s ../raspin_client/raspin raspin
+else
+ echo "no action"
+fi
+nohup sudo python sensor.py > sensor.log 2> sensor_error.log &
+nohup sudo python computing_resource_monitor.py > computing_resource_monitor.log 2> computing_resource_monitor_error.log &
+nohup sudo python physical_controller.py > computing_resource_monitor.log 2> computing_resource_monitor_error.log &
