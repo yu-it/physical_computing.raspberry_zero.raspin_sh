a4a1952dedc87874085b24b3d04fd88d11791fd5
yu-it <yousuke0303@gmail.com>
2018-01-08 13:55:16 +0900

    c

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 current=$(pwd)
 
 cd /home/pi/bin/programs/camera_inst/mjpg-streamer/mjpg-streamer-experimental
-nohup ./mjpg_streamer -i "./input_raspicam.so -fps 10 -x 320 -y 240" -o "./output_http.so -w ./www -p 8080" > cam.log 2> raspin_cam_error.log &
+nohup ./mjpg_streamer -i "./input_raspicam.so -fps 10 -x 320 -y 240 -vf" -o "./output_http.so -w ./www -p 8080" > cam.log 2> raspin_cam_error.log &
 sleep 3
 cd $current
 cd ../raspin
